e2e63a275655621a050b3997811c165c65a2e486
Jose Armin Maningo <armin@isr.com.ph>
2021-06-20 21:08:36 +0800

    added snackbar

--- screens/PantryScreen.js
+++ screens/PantryScreen.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import {FAB} from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Snackbar from 'react-native-snackbar';
 
 import IngredientCategory from '../components/IngredientCategory';
 import IngredientListFooter from '../components/IngredientListFooter';
@@ -15,15 +16,6 @@ import Colors from '../constants/Colors';
 import APIUrls from '../constants/APIUrls';
 
 const PantryScreen = () => {
-  const arr = [
-    {name: 'meat', id: '1'},
-    {name: 'vegetables', id: '2'},
-    {name: 'condiments', id: '3'},
-    {name: 'fruits', id: '4'},
-    {name: 'poultry', id: '5'},
-    {name: 'natives', id: '6', isLast: true},
-  ];
-
   const [isLoading, setIsLoading] = useState(true);
   const [ingredients, setIngredients] = useState([]);
 
@@ -39,18 +31,21 @@ const PantryScreen = () => {
       })
       .catch(error => {
         console.error(error);
+        Snackbar.show({
+          text: 'Please check your internet connection',
+          duration: Snackbar.LENGTH_INDEFINITE,
+          action: {
+            text: 'Retry',
+            textColor: 'green',
+            onPress: () => { 
+              setIsLoading(true);
+              getIngredientsFromWiseCookApi();
+            },
+          },
+        });
       })
       .finally(() => setIsLoading(false));
   };
-
-  if (isLoading) {
-    return (
-      <View
-        style={{flex: 1, justifyContent: 'center', backgroundColor: 'white'}}>
-        <ActivityIndicator size="large" color={Colors.primaryColor} />
-      </View>
-    );
-  }
 
   const renderIngredient = itemData => {
     return (
@@ -60,6 +55,25 @@ const PantryScreen = () => {
       />
     );
   };
+
+  if (isLoading) {
+    return (
+      <View
+        style={{flex: 1, justifyContent: 'center', backgroundColor: 'white'}}>
+        <ActivityIndicator title="connecting" size="large" color={Colors.primaryColor} />
+      </View>
+    );
+  }
+
+  if (!ingredients || ingredients.length <= 0) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={styles.infoText}>
+          Oh no! Something's really wrong here.
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.screen}>
@@ -107,6 +121,12 @@ const styles = StyleSheet.create({
   },
   scrollView: {
     paddingTop: 2,
+  },
+  infoText: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: Colors.primaryColor,
+    paddingHorizontal: 10,
   },
 });
 
