d8761a6d0498bc4317c623178469d7c096154190
Jose Armin Maningo <armin@isr.com.ph>
2021-06-26 16:04:10 +0800

    initial implementation for steps

--- screens/recipeScreens/RecipeViewerScreen.js
+++ screens/recipeScreens/RecipeViewerScreen.js
@@ -75,18 +75,61 @@ const RecipeViewerScreen = props => {
           </View>
         </ImageBackground>
       </View>
+
       <View style={styles.mealDetailContainer}>
         <RecipeInfo iconName="clock-outline" info={recipe.time} />
         <RecipeInfo iconName="bread-slice-outline" info={recipe.servings} />
       </View>
 
-      {/* <View>
-        <Text>Ingredients</Text>
-      </View> */}
-      <Text>Ingredients</Text>
-      <View>
+      <View style={styles.ingredientContainer}>
+        <Text style={styles.ingredientTitle}>Ingredients</Text>
         {recipe.ingredients.map(ingredient => (
-          <Text key={ingredient.id}>{ingredient.name}</Text>
+          <View key={ingredient.id} style={styles.ingredientRowContainer}>
+            <Text style={styles.ingredientEntry}>
+              <Text style={{fontWeight: 'bold'}}>{ingredient.quantity}</Text>
+              <Text>{ingredient.quantity.length > 0 ? ' ' : ''}</Text>
+              <Text>{ingredient.name}</Text>
+            </Text>
+          </View>
+        ))}
+      </View>
+
+      <View style={styles.ingredientContainer}>
+        <Text style={styles.ingredientTitle}>Steps</Text>
+        {recipe.instructions.map(instruction => (
+          <View key={instruction.id} style={styles.ingredientRowContainer}>
+            {/* <Text
+              style={{
+                textAlign: 'center',
+                backgroundColor: 'green',
+                fontSize: 10 - 2 * 5, //... One for top and one for bottom alignment
+                lineHeight: 5 - ( 2 * 5), //... One for top and one for bottom alignment
+              }}>
+              1
+            </Text> */}
+            <Text style={styles.ingredientEntry}>
+              <View
+                style={{
+                  width: 18,
+                  height: 18,
+                  justifyContent: 'center',
+                  borderRadius: 18 / 2,
+                  backgroundColor: Colors.primaryColor,
+                }}>
+                <Text
+                  style={{
+                    alignSelf: 'center',
+                    color: 'white',
+                    fontSize: 12,
+                  }}>
+                  {instruction.step}
+                </Text>
+               
+              </View>
+              <Text>{'  '}</Text>
+              <Text>{instruction.ins}</Text>
+            </Text>
+          </View>
         ))}
       </View>
     </ScrollView>
@@ -130,6 +173,38 @@ const styles = StyleSheet.create({
     height: 22,
     alignItems: 'center',
     backgroundColor: '#ebe4e4',
+  },
+  ingredientContainer: {
+    marginHorizontal: 10,
+    marginTop: 8,
+    borderBottomWidth: 0.2,
+    borderBottomColor: Colors.gray,
+    paddingBottom: 20,
+    marginBottom: 10,
+  },
+  ingredientTitle: {
+    marginBottom: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  ingredientRowContainer: {
+    paddingVertical: 10,
+    marginHorizontal: 10,
+    borderColor: Colors.gray,
+    borderStyle: 'dashed',
+    borderRadius: 1,
+    borderWidth: 0,
+    borderTopWidth: 0.2,
+  },
+  stepNumContainer: {
+    alignContent: 'center',
+    backgroundColor: Colors.primaryColor,
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+  stepNum: {
+    color: 'white',
   },
 });
 
