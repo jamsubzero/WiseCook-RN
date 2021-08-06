f3b4ea418051750242f87abc33c44fb2d284c7b5
Jose Armin Maningo <armin@isr.com.ph>
2021-08-06 14:39:01 +0800

    updated URL for AWS

--- constants/APIUrls.js
+++ constants/APIUrls.js
@@ -1,10 +1,9 @@
 export default {
-    INGREDIENT_LIST_URL: "http://armin.isr.co.jp:8080/ingredient/all",
-    RECIPE_BY_INGREDIENT_URL: "http://armin.isr.co.jp:8080/recipe/by-ing?ings=",
-    RECIPE_DETAILS_URL: "http://armin.isr.co.jp:8080/recipe?id=",
-    HOME_CONTENT_URL: "http://armin.isr.co.jp:8080/recipe/recipe-home?ings=",
-    RECIPE_SEARCH_WITH_FILTER_URL: "http://armin.isr.co.jp:8080/recipe/by-ing/with-key?ings=",
-    //http://armin.isr.co.jp:8080/recipe/by-ing/with-key?ings=2,4,5&keywords=lunch
-    RECIPE_SEARCH_BY_NAME_URL: "http://armin.isr.co.jp:8080/recipe/by-name?keyword=",
-    FAVORITES_URL: "http://armin.isr.co.jp:8080/recipe/favorites?recIds="
+    INGREDIENT_LIST_URL: "http://wisecookapi.us-east-2.elasticbeanstalk.com/ingredient/all",
+    RECIPE_BY_INGREDIENT_URL: "http://wisecookapi.us-east-2.elasticbeanstalk.com/recipe/by-ing?ings=",
+    RECIPE_DETAILS_URL: "http://wisecookapi.us-east-2.elasticbeanstalk.com/recipe?id=",
+    HOME_CONTENT_URL: "http://wisecookapi.us-east-2.elasticbeanstalk.com/recipe/recipe-home?ings=",
+    RECIPE_SEARCH_WITH_FILTER_URL: "http://wisecookapi.us-east-2.elasticbeanstalk.com/recipe/by-ing/with-key?ings=",
+    RECIPE_SEARCH_BY_NAME_URL: "http://wisecookapi.us-east-2.elasticbeanstalk.com/recipe/by-name?keyword=",
+    FAVORITES_URL: "http://wisecookapi.us-east-2.elasticbeanstalk.com/recipe/favorites?recIds="
 }
